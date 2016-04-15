fef6fe72b191afe953a2a5b004b06b205f1a8fc9
Jon Packles <jon.packles@gmail.com>
2016-04-14 20:49:29 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    $('#fullpage').fullpage({
+    	scrollingSpeed: 600,
+    });
+
+});
+
+
+
+
+// ORIGINAL SMOOTHSCROLL. GOES ONLY FROM DIV 1 TO DIV 2
+// 	function scrollToAnchor(aid){
+//     var aTag = $("a[name='"+ aid +"']");
+//     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+// }
+
+// 	$('html').keydown(function() {
+// 	   scrollToAnchor('imageTwo');
+
+
+// 	});
+
+// END ORIGINAL SMOOTHSCROLL. GOES ONLY FROM DIV 1 TO DIV 2
+
+
+
+
+
+
+
+// if(e.keyCode == 40){
+//             $(this).css({'background':'red'});  
+//         }
+//         if(e.keyCode == 121){
+//             $(this).css({'background':'yellow'});
+//         }
