9f4e85c1124ea1cf1e16151392797cf685357277
Jon Packles <jon.packles@gmail.com>
2016-04-19 00:46:04 -0400

    oh yeah

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $('#fullpage').fullpage({
-    	scrollingSpeed: 500,
+    	scrollingSpeed: 200,
     	slidesNavigation: true,
         slidesNavPosition: 'bottom',
     	verticalCentered: false,
